5a1058af7d0f230dcf2b1621991f5785c19b076a
Courtney Macgregor <cjoss90@hotmail.com>
2025-11-22 21:23:11 +1000

    feat(models): add User model and relationship placeholder

--- server/models.py
+++ server/models.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+
+    items = db.relationship("Item", backref="user", lazy=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
