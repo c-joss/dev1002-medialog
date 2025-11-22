e19a9d04cf5e4813cc064c88dba064ee68d47ba5
Courtney Macgregor <cjoss90@hotmail.com>
2025-11-22 21:29:41 +1000

    feat(models): add Category model

--- server/models.py
+++ server/models.py
@@ -12,3 +12,14 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    items = db.relationship("Item", backref="category", lazy=True)
+
+    def __repr__(self):
+        return f"<Category {self.name}>"
